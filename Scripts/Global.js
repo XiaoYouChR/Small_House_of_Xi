6849966c016f80b79b3d08d2434ab0c4b92e6f42
XiaoYouChR <XiaoYouChR@outlook.com>
2022-08-10 14:53:08 +0800

    Personal Backup

--- Scripts/Global.js
+++ Scripts/Global.js
@@ -1,50 +1,167 @@
-// const BackBtn = document.getElementById("BackBtn");
-// const ForwardBtn = document.getElementById("ForwardBtn")
-// const Control = document.getElementById("ControlBtn")
+const { ipcRenderer, app } = require("electron")
 
 const MainDisplay = document.getElementById("MainDisplay")
 const Status = document.getElementById("Status")
 const DownloadDisplay = document.getElementById("DownloadDisplay")
+const DefaultTask = document.getElementById("DefaultTask")
+const SideBar = document.getElementById("SideBar")
+const ControlTools = document.getElementById("ControlTools")
+
+var TaskList = []
+var TaskNum = []
+
+// 接收来自主进程的下载信号
+ipcRenderer.on("StartDownload", (event, args) => {
+    // 添加任务编号
+    const TaskID = TaskNum.length
+    TaskList[args] = TaskID
+    TaskNum.push(TaskID)
+    var Task = DefaultTask.cloneNode(true)
+    DownloadDisplay.insertBefore(Task, DefaultTask)
+    Task.id ="Task-" + TaskID
+    console.log("Init:", args ,TaskID, TaskList, TaskNum, TaskList[args])
+    // 回复任务编号给主进程
+    ipcRenderer.send("TaskID", TaskID)
+
+    // 绑定函数
+    console.log("#" + Task.id+" #PauseBtn")
+    const PauseBtn = document.querySelector("#" + Task.id+" #PauseBtn")
+    console.log(PauseBtn)
+    const CancelBtn = document.querySelector("#" + Task.id+" #CancelBtn")
+    console.log(CancelBtn)
+
+    PauseBtn.onclick = function () {
+        ipcRenderer.send("PauseTask")
+    }
+
+    CancelBtn.onclick = function () {
+        Task.remove()
+        ipcRenderer.send("CancelTask")
+    }
+})
+
+// 接收来自主进程的初始化下载任务信号
+// ipcRenderer.on("InitTask", (event, args) => {
+//     const TaskID = args[0]
+//
+//
+//     console.log("InitTask:", TaskID, FileName)
+//     // 获取Icon
+//     // const Icon = getFileIcon(FileName)
+//     // console.log(Icon)
+//     // 获取对象
+//     const TaskSelector = "#Task-" + TaskID
+//
+//     const FileIcon = document.querySelector(TaskSelector + " #FileIcon")
+//     // 更新界面
+//
+//     FileSizeSpan.innerHTML = "0B/" + FileSize + "B"
+//     // FileIcon.src = Icon
+// })
+
+// 接收来自主进程的更新界面信号
+ipcRenderer.on("UpdateData", (event, args) => {
+    const FileName = args[4]
+    const TaskID = TaskList[FileName]
+    const speed = args[0] + "B/s"
+    const progress = args[1]
+    const ReceivedAndTotal = args[2] + "B/" + args[3] + "B"
+    console.log("UpdateData:", FileName, TaskID, speed, ReceivedAndTotal)
+    // 获取对象
+    const TaskSelector = "#Task-" + TaskID
+    const FileSizeSpan = document.querySelector(TaskSelector + " #DownloadedSize")
+    const ProgressBar = document.querySelector(TaskSelector + " #ProgressBar")
+    const DownloadSpeed = document.querySelector(TaskSelector + " #DownloadSpeed")
+    const FileNameSpan = document.querySelector(TaskSelector + " #FileName")
+    // 更新界面
+    FileNameSpan.innerHTML = "文件名: " + FileName
+    FileSizeSpan.innerHTML = ReceivedAndTotal
+    ProgressBar.value = progress
+    DownloadSpeed.innerHTML = speed
+})
+
 // 监听器
 MainDisplay.addEventListener("did-start-loading", loadstart)
 MainDisplay.addEventListener("did-frame-navigate", frameNavigate)
 
 function loadstart() {
     Status.innerHTML = "Loading..."
-    console.log("LoadStart");
+    console.log("LoadStart")
 }
 
 function frameNavigate(e) {
     Status.innerHTML = "Response Code:" + e.httpResponseCode
     console.log("FrameNavigate")
 }
+
 // 实现Tools的函数
 function DownloadManager() {
     console.log(MainDisplay.style.display)
     MainDisplay.style.display = "none"
     DownloadDisplay.style.display = "inline-flex"
 }
+
 // 控制WebView函数
 function Back() {
     MainDisplay.style.display = ""
     DownloadDisplay.style.display = "none"
-    MainDisplay.goBack();
-    console.log("向后");
+    MainDisplay.goBack()
+    console.log("向后")
 }
 
 function Forward() {
     MainDisplay.style.display = ""
     DownloadDisplay.style.display = "none"
-    MainDisplay.goForward();
-    console.log("向前");
+    MainDisplay.goForward()
+    console.log("向前")
 }
 
 function Refresh() {
-    MainDisplay.reload();
+    MainDisplay.reload()
 }
 
 function LoadURL(url) {
     MainDisplay.style.display = ""
     DownloadDisplay.style.display = "none"
-    MainDisplay.loadURL(url);
+    MainDisplay.loadURL(url)
+}
+
+function OpenBeggingPage () {
+    LoadURL('https://seelevollerei.sharepoint.com/:x:/s/seelevollerei/Ed_xUqHmmcVMiTaZGBGeXrMBSUJIJHvTJPIgjAgM_QmFfQ?e=9cvf8R')
+    // MainDisplay.executeJavaScript("const Header = document.getElementById(\"Header\")\n" +
+    //     "const Ribbon = document.getElementById(\"Ribbon\")\n" +
+    //     "const BusinessBar = document.getElementById(\"BusinessBar\")\n" +
+    //     "const AdditionalBars = document.getElementById(\"AdditionalBars\")\n" +
+    //     "Header.remove()\n" +
+    //     "Ribbon.remove()\n" +
+    //     "BusinessBar.remove()\n" +
+    //     "AdditionalBars.remove()", false)
+}
+
+// const Header = document.getElementById("Header")
+// const Ribbon = document.getElementById("Ribbon")
+// const BusinessBar = document.getElementById("BusinessBar")
+// const AdditionalBars = document.getElementById("AdditionalBars")
+// Header.remove()
+// Ribbon.remove()
+// BusinessBar.remove()
+// AdditionalBars.remove()
+
+// 隐藏功能
+function そら() {
+    var HideTools = document.createElement("div")
+    SideBar.insertBefore(HideTools, ControlTools)
+    HideTools.className = "Tools"
+    HideTools.id = "HideTools"
+
+    HideTools.innerHTML = '        <p>隐藏功能</p>\n' +
+        '        <a href="javascript:" onclick="LoadURL(\'https://seelevollerei-my.sharepoint.com/:f:/g/personal/rbq_seelevollerei_onmicrosoft_com/Ep5HkKkJ3whHg683n9NxE4UByEyBXqOEPWZ-EDvkZ608Dw\')">\n' +
+        '            <div class="Option"><img src="./Resources/存款.svg" alt="">鸢鸢の仓库</div>\n' +
+        '        </a>' +
+        '        <a href="javascript:" onclick="LoadURL(\'https://seelevollerei.sharepoint.com/:f:/s/seelevollerei/EoWF1q3ubPNLpNJfdcvLY5gBws2t3s58x4-tT99dayH-2Q\')">\n' +
+        '            <div class="Option"><img src="./Resources/关于.svg" alt="">睎の博物馆</div>\n' +
+        '        </a>' +
+        '        <a href="javascript:" onclick="LoadURL(\'https://seelevollerei.sharepoint.com/:f:/s/seelevollerei/EtHpnGIU-pBIkei2gN5ZMqYBxS0EzVNrXIYLxqZF39tgnQ\')">\n' +
+        '            <div class="Option"><img src="./Resources/分享.svg" alt="">资源共享中心</div>\n' +
+        '        </a>'
 }
--- index.js
+++ index.js
@@ -1,34 +1,97 @@
-const { app, BrowserWindow, session} = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 // const path = require('path');
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      // preload: path.join(__dirname, 'preload.js'),
-      webviewTag: true, // 允许 WebView
-      nodeIntegration: true, // 不加，那就都别想活着！
-    },
-  });
+    const win = new BrowserWindow({
+        minWidth: 1200,
+        minHeight: 800,
+        webPreferences: {
+            // preload: path.join(__dirname, 'preload.js'),
+            webviewTag: true, // 允许 WebView
+            nodeIntegration: true, // 不加，那就都别想活着！
+            contextIsolation: false  //  不加，那就都别想活着！
+        },
+    });
 
-  win.loadFile('index.html');
+    win.removeMenu()
+    win.loadFile('index.html');
+
+
+// 下载实现
+    win.webContents.session.on('will-download', (event, item, webContents) => {
+
+        win.webContents.send("StartDownload", item.getFilename())
+
+        item.TaskID = 0
+        var prevReceivedBytes = 0
+
+        // 获取Task编号
+        ipcMain.on("TaskID", (e, args) =>{
+            item.TaskID = args
+            console.log(item.TaskID)
+        })
+        // 初始化了家人们
+        // const FileName = item.getFilename()
+        // const FileSize = item.getTotalBytes()
+
+        // win.webContents.send("InitTask", [item.TaskID, item.getFilename(), item.getTotalBytes()])
+
+        ipcMain.on("PauseTask", () => {
+            if (item.isPaused() === true) {
+                item.resume()
+            } else {
+                item.pause()
+            }
+        })
+
+        ipcMain.on("CancelTask", () => {
+            item.cancel()
+        })
+
+        item.on('updated', (event, state) => {
+            const receivedBytes = item.getReceivedBytes()
+            // 计算每秒下载的速度
+            item.speed = receivedBytes - prevReceivedBytes
+            prevReceivedBytes = receivedBytes
+            // 进度
+            item.progress = item.getReceivedBytes() / item.getTotalBytes()
+
+            // 更新速度
+            win.webContents.send("UpdateData", [item.speed, item.progress, receivedBytes, item.getTotalBytes(), item.getFilename()] )
+            // win.webContents.send("UpdateData", item )
+
+            if (state === 'interrupted') {
+                console.log('Download is interrupted but can be resumed')
+            } else if (state === 'progressing') {
+                if (item.isPaused()) {
+                    console.log('Download is paused')
+                } else {
+                    console.log(`Received bytes: ${item.getReceivedBytes()}`)
+                }
+            }
+        })
+        item.once('done', (event, state) => {
+            if (state === 'completed') {
+                console.log('Download successfully')
+            } else {
+                console.log(`Download failed: ${state}`)
+            }
+        })
+    })
 };
 
 app.whenReady().then(() => {
-  createWindow();
+    createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
-
-// 下载实现
